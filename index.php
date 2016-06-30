144bcaed0e799d57a4a80c3c6941440b485052bb
webler <wolodymyr.ch@gamil.com>
2016-06-30 23:06:45 +0300

    fix connect

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 require_once 'bootstrap.php';
 
-$user = new User();
-$user->setName('Igo');
-echo  $user->getName();
+//$user = new User();
+//$user->setName('Igo');
+//echo  $user->getName();
+echo 'test';
