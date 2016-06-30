3aabea8b742d294a1dd04aa5e3aed9c01f4b792d
webler <wolodymyr.ch@gamil.com>
2016-06-30 22:21:19 +0300

    fix connect

--- bootstrap.php
+++ bootstrap.php
@@ -22,12 +22,8 @@ $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
 // the connection configuration
 $dbParams = array(
-    'driver'   => 'pdo_mysql',
-    'user'     => $url["user"],
-    'password' => $url["pass"],
-    'dbname'   => substr($url["path"], 1),
-    'host'     => $url["host"],
-);
+    'url' => getenv("CLEARDB_DATABASE_URL"),
+    );
 
 // obtaining the entity manager
 $entityManager = EntityManager::create($dbParams, $config);
